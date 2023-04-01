e8d0b358acf5ac457705c3689713c728f5eb380d
Mohan <94776837+Mohanendla@users.noreply.github.com>
2023-04-02 00:17:32 +0530

    Updated

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,12 @@
+
 import React from 'react';
 
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button className={fix-box} onClick={() => selectNextBackground({background: background})}>
+      {label}
     </button>
   )
 }
